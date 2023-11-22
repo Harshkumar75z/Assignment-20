18b71e3c47f4df51dc5ba7795fb95934a0b683cb
Harsh Kumar <136227178+Harshkumar75z@users.noreply.github.com>
2023-11-22 15:18:38 +0530

    Add files via upload

---
+++ Ques-1.cpp
@@ -0,0 +1,8 @@
+// Calculate the time complexity for the following code snippet.
+for(int i = 0; i < n; i++) {
+for(int j = 0; j * j < n; j++) {
+cout << “PhysicsWallah ”;
+    }
+}
+
+Answer = O(n*sqrt n)
